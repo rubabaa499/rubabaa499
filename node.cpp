5d44f390c84324275cf8f11fadc26a88cd4b9602
Rubaba Rahman <89207252+rubabaa499@users.noreply.github.com>
2021-08-19 21:36:53 +0600

    Add files via upload

---
+++ node.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstdlib>
+#include<stdlib.h>
+using namespace std;
+
+ struct Node{
+  int data;
+  struct Node*link;
+ };
+
+ int main(){
+
+ struct Node*head= NULL;
+
+ head= (struct Node*)malloc(sizeof(struct Node));
+ head->data= 45;
+ head-> link=NULL;
+
+ struct Node*current=NULL;
+
+ current= (struct Node*)malloc(sizeof(struct Node));
+ current->data= 20;
+ current->link= NULL;
+
+ head->link=current;
+
+ current= (struct Node*)malloc(sizeof(struct Node));
+ current->data= 10;
+ current->link= NULL;
+ head->link->link =current;
+
+ cout<<"first node address"<<head<<&head->data<< endl;
+ cout<< "first node data "<< head->data<< endl;
+ cout<< "first node link "<< head->link<< endl;
+ cout<< "2nd node data "<< head->link->data<< endl;
+ cout<< "2nd node link "<< head->link->link<< endl;
+ cout<< "3rd node data "<< head->link->link->data<< endl;
+ cout<< "3rd node data "<< head->link->link->link<< endl;
+
+ return 0;
+
+
+
+ }
